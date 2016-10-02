766c61b383ee0ae5e3c1286c867c275c9a60dc66
fousmane <ousmanefouchimi1990@gmail.com>
2016-10-01 21:01:57 -0600

    desc:Added Linked folder, with MainActivity, SignUpActivity and Login Activity

---
+++ Linked/app/src/main/java/afinal/nanodegree/com/linked/LoginActivity.java
@@ -0,0 +1,28 @@
+package afinal.nanodegree.com.linked;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class LoginActivity extends AppCompatActivity {
+    private TextView mSignUpTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mSignUpTextView = (TextView) findViewById(R.id.signUpText);
+        mSignUpTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Linked/app/src/main/java/afinal/nanodegree/com/linked/SignUpActivity.java
@@ -0,0 +1,13 @@
+package afinal.nanodegree.com.linked;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
